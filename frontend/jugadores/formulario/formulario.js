7bb7156a863c2a63fb148bd1eeaa392432978882
bequer <bhuaman@fi.uba.ar>
2024-11-30 17:32:37 -0300

    Creo una funcion de solicitud para el fetch y la autenticacion

--- frontend/jugadores/formulario/formulario.js
+++ frontend/jugadores/formulario/formulario.js
@@ -4,13 +4,26 @@ const BASIC_AUTH_PASS = 'admin123';
 const credenciales = btoa(`${BASIC_AUTH_USER}:${BASIC_AUTH_PASS}`);
 
 
-fetch("http://localhost:3000/jugadores", {
-    method: 'GET',
-    headers: {
+function solicitud_Autenticacion(url, method = 'GET', body = null) {
+    const headers = {
         'Authorization': `Basic ${credenciales}`,
+        'Content-Type': 'application/json',
+    };
+    if (body) { // Si el método es POST, PUT o PATCH, incluye el cuerpo de la solicitud
+        return fetch(url, {
+            method: method,
+            headers: headers,
+            body: JSON.stringify(body),
+        });
     }
-})
+    return fetch(url, {  // Si no hay cuerpo (GET, DELETE), solo haz la solicitud con los headers
+        method: method,
+        headers: headers,
+    });
+}
+
+solicitud_Autenticacion("http://localhost:3000/jugadores")
     .then(response => response.json())
     .then(datos => {
-        console.log("la base de datos esta funcionando!!!",datos)
-    })  
+        console.log("Datos de jugadores:", datos);
+    })
