4c7fdf27493747886642703bacb0d77bab58af74
bequer <bhuaman@fi.uba.ar>
2024-12-12 13:12:23 -0300

    modifico el flujo de entrada de los datos

--- frontend/jugadores/agregar_jugador/jugador.js
+++ frontend/jugadores/agregar_jugador/jugador.js
@@ -28,8 +28,7 @@ function cerrarModal() {
     modal.classList.remove('is-active'); 
 }
 function crearJugador() {
-
-    const form = document.getElementById('myForm');
+    const form = document.getElementById('formulario');
     const inputs = form.querySelectorAll('input[required]');
     let formIsValid = true;
 
@@ -45,37 +44,35 @@ function crearJugador() {
     if (!formIsValid) {
         event.preventDefault();
         alert('Por favor, completa todos los campos requeridos.');
+    }else{
+        event.preventDefault();
+        const Nombre = document.getElementById('nombre').value;
+        const Contrasena = document.getElementById('contraseña').value;
+        const Email = document.getElementById('email').value;
+        const Nivel = document.getElementById('nivel').value;
+        const Estado = document.getElementById('estado').value;
+        const Pais = document.getElementById('pais').value;
+        const Servidor = document.getElementById('servidor').value;
+        let datos_jugador = {
+            nombre: Nombre, 
+            contrasena: Contrasena, 
+            email: Email,
+            nivel: parseInt(Nivel), 
+            estado: Estado, 
+            pais: Pais, 
+            servidor: Servidor
+        };
+        solicitud_Autenticacion("http://localhost:5000/jugadores",'POST',datos_jugador)
+            .then(response => {
+                if (response.status === 201) {
+                    alert('Jugador creado con exito');
+                    cerrarModal();
+                    limpiarModal();
+                    gestion_jugadores();
+                }else {
+                    alert('No se pudo crear');
+                    limpiarModal();
+                }
+            })
     }
-    event.preventDefault();
-    const Nombre = document.getElementById('nombre').value;
-    const Contrasena = document.getElementById('contraseña').value;
-    const Email = document.getElementById('email').value;
-    const Nivel = document.getElementById('nivel').value;
-    const Estado = document.getElementById('estado').value;
-    const Pais = document.getElementById('pais').value;
-    const Servidor = document.getElementById('servidor').value;
-
-    let datos_jugador = {
-        nombre: Nombre, 
-        contrasena: Contrasena, 
-        email: Email,
-        nivel: parseInt(Nivel), 
-        estado: Estado, 
-        pais: Pais, 
-        servidor: Servidor
-    };
-    
-    solicitud_Autenticacion("http://localhost:5000/jugadores",'POST',datos_jugador)
-        .then(response => {
-            if (response.status === 201) {
-                alert('Jugador creado con exito');
-                cerrarModal();
-                limpiarModal();
-                gestion_jugadores();
-            }else {
-                alert('No se pudo crear');
-                limpiarModal();
-                cerrarModal();
-            }
-        })
 }
